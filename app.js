78cc889ef38fa8c73a30d65f9f2402ab823f0518
Leonardo Rafael Guanuco <leomusicocompositor@gmail.com>
2023-08-21 23:02:36 -0300

    hola

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "public")));
 
+app.set('puerto',process.env.PORT || 3001);
+
 //rutas
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "views/home.html"));
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Success");
-});
+app.listen(app.get('puerto'), ()=> console.log(`Servidor corriendo de manera correcta${app.get('puerto')}`));
